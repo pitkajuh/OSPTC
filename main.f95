2e56bda53e32eecfd35a70fd4b48170f26650bdb
pitkajuh <github_pj@protonmail.com>
2025-07-06 15:01:26 +0300

    ok

--- main.f95
+++ main.f95
@@ -2,7 +2,7 @@ program main
   implicit none
 
   character(len=5) :: line
-  character(len=10) :: line2
+  character(len=75) :: line2
   integer(8) :: ios, unit
   character(len=10) :: a1, a2, a3, a4, a5, a6, a7
   ! character(len=12) :: a1
@@ -16,9 +16,11 @@ program main
      if (line == "23501") exit
   end do
 
+
+
   do
      ! read(unit, '(A75)', iostat=ios) line, line2
-     read(unit, '(A10)', iostat=ios) line2
+     read(unit, '(A75)', iostat=ios) line2
      ! read(unit, '(A22)', iostat=ios) line, line2
      ! read(unit, '(a)', iostat=ios) line
      ! read(unit, '(a)', iostat=ios) a1, a2, a3, a4, a5, a6, a7
