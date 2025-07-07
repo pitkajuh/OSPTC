425874b96413e232ac3df054ad467601c6cf90c4
pitkajuh <github_pj@protonmail.com>
2025-07-07 20:32:03 +0300

    ok

--- main.f95
+++ main.f95
@@ -3,48 +3,48 @@
 module read_endf
   implicit none
 contains
-  subroutine read_begin(z)
+  subroutine read_begin(z, ios)
     implicit none
 
     integer :: z
-    integer :: iostat
+    integer :: ios
     character(75) :: line
 
     do
-       read(z, '(A75)', iostat=iostat) line
+       read(z, '(A75)', iostat=ios) line
        ! print *, line
        if(trim(line(2:5))/="ENDF" .and. trim(line(72:75))=="0  0") exit
     end do
 
   end subroutine read_begin
 
-  subroutine read_records(z)
+  subroutine read_records(z, ios)
     implicit none
 
     integer :: z
-    integer :: iostat
+    integer :: ios
     character(75) :: line
 
     do
-       read(z, '(A75)', iostat=iostat) line
+       read(z, '(A75)', iostat=ios) line
        ! print *, line
        if(line(73:75)=="  0") exit
     end do
 
   end subroutine read_records
 
-  subroutine read_header(z)
+  subroutine read_header(z, ios)
     implicit none
 
     integer :: z
-    integer :: iostat
+    integer :: ios
     integer :: n
     character(75) :: line
 
     n=1
 
     do
-       read(z, '(A75)', iostat=iostat) line
+       read(z, '(A75)', iostat=ios) line
        print *, line
        if(n==3) exit
        n=n+1
@@ -59,25 +59,16 @@ contains
     integer :: ios, z
     z=1
 
-    open(z, file=tape_name, status="old", action="read")
-    call read_begin(z)
-    call read_header(z)
-    call read_records(z)
-    print *, ""
-    call read_header(z)
-    call read_records(z)
-    print *, ""
-    call read_header(z)
-    call read_records(z)
-    print *, ""
-    call read_header(z)
-    call read_records(z)
-    print *, ""
-    call read_header(z)
-    call read_records(z)
-    print *, ""
-    call read_header(z)
-    call read_records(z)
+    open(z, file=tape_name, status="old", action="read", iostat=ios)
+    call read_begin(z, ios)
+
+    do
+       if(ios<0) exit
+       call read_header(z, ios)
+       call read_records(z, ios)
+       print *, ""
+    end do
+
     close(z)
   end function read_tape
 end module read_endf
@@ -86,7 +77,6 @@ program main
   use read_endf
   implicit none
 
-  ! character(len=5) :: line
   character(len=75) :: line
   character(len=75) :: line2
   integer(8) :: ios, z
@@ -97,50 +87,5 @@ program main
   integer :: res
 
   res=read_tape('cross-sections/photoat-007_N_000.endf')
-  ! call read_tape("cross-sections/photoat-007_N_000.endf")
-
-  ! character(len=12) :: a1
-  ! character(len=11) :: a2, a3, a4, a5, a6
-  ! character(len=8) :: a7
-
-
-  ! z = 7
-  ! open(z, file="cross-sections/photoat-007_N_000.endf", status='old', action='read', iostat=ios)
-
-  ! do
-  !    read(z, '(A75)', iostat=ios) line
-  !    if(trim(line(2:5))/="ENDF" .and. trim(line(72:75))=="0  0") exit
-  ! end do
-
-  ! do
-  !    read(z, '(A75)', iostat=ios) line
-  !    print *, line
-  !    if (ios < 0) exit
-  ! end do
-
-
-
-!   do
-!      ! read(z, "(2E11.0,4I11,I4,I2,I3,I5)") &
-!      !      C1,C2,L1,L2,N1,N2,MAT,MF,MT,NS
-
-!      ! print *, "(2E11.0,4I11,I4,I2,I3,I5)"
-! ! 100  format(2E11.0,4I11,I4,I2,I3,I5)
-!      ! if (ios < 0) exit
-
-!      ! read(z, '(A75)', iostat=ios) line, line2
-!      read(z, '(A75)', iostat=ios) line2
-!      ! read(z, '(A22)', iostat=ios) line, line2
-!      ! read(z, '(a)', iostat=ios) line
-!      ! read(z, '(a)', iostat=ios) a1, a2, a3, a4, a5, a6, a7
-!      if (ios < 0) exit
-!      ! if (line == "23501") exit
-
-!      print *, '@', trim(line2), '@', '@'
-!      ! print *, '@', trim(line), '@', trim(line2), '@'
-!      ! print *, trim(a1), a2, a3, a4, a5, a6, a7
-!   end do
-
-  ! close(z)
 
 end program main
