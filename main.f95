dcd0eb2b23fbee4f4af2b426002116a31d880e87
pitkajuh <github_pj@protonmail.com>
2025-07-07 17:33:34 +0300

    ok

--- main.f95
+++ main.f95
@@ -1,4 +1,36 @@
+! subroutine read_begin(ios)
+!   implicit none
+
+!   integer :: ios
+!   character(75) :: line
+
+!   do
+!      read(1, '(A75)', iostat=ios) line
+!      if(trim(line(2:5))/="ENDF" .and. trim(line(72:75))=="0  0") exit
+!   end do
+
+! end subroutine read_begin
+
+module read_endf
+  implicit none
+contains
+
+  integer function read_tape(tape_name)
+    implicit none
+
+    character :: tape_name
+    integer :: ios
+    ! integer :: read_tape
+    ! character(75) :: line
+
+    open(1, file=tape_name, status="old", action="read", iostat=ios)
+    close(1)
+    ! read_tape=0.0
+  end function read_tape
+end module read_endf
+
 program main
+  use read_endf
   implicit none
 
   ! character(len=5) :: line
@@ -9,25 +41,27 @@ program main
   ! character(10) :: C1, C2, L1, L2, N1, N2, MAT, MF, MT, NS
   real(kind(1.d0)) :: C1, C2!, L1, L2, N1, N2, MAT, MF, MT, NS
   integer :: L1, L2, N1, N2, MAT, MF, MT, NS
+  integer :: res
+
+  res=read_tape("cross-sections/photoat-007_N_000.endf")
 
   ! character(len=12) :: a1
   ! character(len=11) :: a2, a3, a4, a5, a6
   ! character(len=8) :: a7
-  character(4) :: endf
-  z = 7
-  open(z, file="cross-sections/photoat-007_N_000.endf", status='old', action='read', iostat=ios)
 
-  do
-     read(z, '(A75)', iostat=ios) line
-     endf=trim(line(2:5))
-     if(endf/="ENDF" .and. trim(line(72:75))=="0  0") exit
-  end do
+
+  ! z = 7
+  ! open(z, file="cross-sections/photoat-007_N_000.endf", status='old', action='read', iostat=ios)
+
+  ! do
+  !    read(z, '(A75)', iostat=ios) line
+  !    if(trim(line(2:5))/="ENDF" .and. trim(line(72:75))=="0  0") exit
+  ! end do
 
   ! do
   !    read(z, '(A75)', iostat=ios) line
   !    print *, line
-  !    ! exit
-  !    if (line == "23501") exit
+  !    if (ios < 0) exit
   ! end do
 
 
@@ -53,6 +87,6 @@ program main
 !      ! print *, trim(a1), a2, a3, a4, a5, a6, a7
 !   end do
 
-  close(z)
+  ! close(z)
 
 end program main
