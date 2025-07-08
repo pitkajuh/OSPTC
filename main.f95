95a0e8787673553b67c8a1697ff2292a15fda73a
pitkajuh <github_pj@protonmail.com>
2025-07-08 19:36:38 +0300

    ok

--- main.f95
+++ main.f95
@@ -1,10 +1,12 @@
 program main
   use read_endf
-  use surface_type
+  use planex_type
   implicit none
 
   integer :: res
-
+  type(surface) :: a3=surface(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+  type(surface) :: a
+  type(planex) :: a1=planex(0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0)
   res=read_tape('cross-sections/photoat-007_N_000.endf')
   res=read_tape('cross-sections/photoat-011_Na_000.endf')
   res=read_tape('cross-sections/photoat-022_Ti_000.endf')
--- surface_type.f95
+++ surface_type.f95
@@ -3,25 +3,32 @@ module surface_type
 
   type :: surface
      ! For general quadratic surface S(x, y, z)=Ax^2+By^2+Cz^2+Dxy+Eyz+F zx+Gx+Hy+Iz+J
-  real(kind(1.d0)) value, A, B, C, D, E, F, G, H, I, J, L, M, K
-
-  ! A=0
-  ! B=0
-  ! C=0
-  ! D=0
-  ! E=0
-  ! F=0
-  ! G=0
-  ! H=0
-  ! I=0
-  ! J=0
-  ! L=0
-  ! M=0
-  ! K=0
+     real(kind(1.d0)) :: value1=0, A=0, B=0, C=0, D=0, E=0, F=0, G=0, H=0, I=0, J=0, L=0, M=0, K=0
   end type surface
 
+  ! module planex_type
+  ! use surface_type
+  ! implicit none
 
+  type, extends(surface) :: planex
 
-
+  end type planex
 
 end module surface_type
+
+! module planex_type
+!   use surface_type
+!   implicit none
+
+!   type, extends(surface) :: planex
+
+!   end type planex
+! contains
+!   subroutine create_plane(v)
+!     implicit none
+!     real(kind(1.d0)) :: v
+!     ! %value1=v
+!     ! %G=1
+!     ! %J=-v
+!   end subroutine create_plane
+! end module planex_type
