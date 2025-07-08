e9178d1cdd2c58921a3cf1a48ac03f20a1ad5313
pitkajuh <github_pj@protonmail.com>
2025-07-08 19:55:36 +0300

    ok

--- surface_type.f95
+++ surface_type.f95
@@ -6,13 +6,20 @@ module surface_type
      real(kind(1.d0)) :: value1=0, A=0, B=0, C=0, D=0, E=0, F=0, G=0, H=0, I=0, J=0, L=0, M=0, K=0
   end type surface
 
-  ! module planex_type
-  ! use surface_type
-  ! implicit none
-
   type, extends(surface) :: planex
-
+   contains
+     procedure :: create => create_planex
   end type planex
+
+contains
+
+  subroutine create_planex(this, v)
+    class(planex), intent(inout) :: this
+    real(kind(1.d0)), intent(in) :: v
+    this%value1=v
+    this%G=1
+    this%J=-v
+  end subroutine create_planex
 
 end module surface_type
 
