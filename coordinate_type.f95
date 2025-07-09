d6b607e1fd84c76d3a61d4dcf7a2b5952d1f6672
pitkajuh <github_pj@protonmail.com>
2025-07-09 20:05:09 +0300

    ok

--- coordinate_type.f95
+++ coordinate_type.f95
@@ -1,10 +1,9 @@
 module coordinate_type
+  use random
   implicit none
 
   type :: coordinate
      real :: x, y, z
-   contains
-
   end type coordinate
 
 contains
@@ -13,5 +12,22 @@ contains
     type(coordinate), intent(in) :: this
     print *, this%x, this%y, this%z
   end subroutine show
+
+  function distance(from, to)
+    type(coordinate) :: from, to
+    real :: distance
+    distance=((to%x-from%x)**2+(to%y-from%y)**2+(to%z-from%z)**2)**0.5
+  end function distance
+
+  function random_emission_direction()
+    type(coordinate) :: random_emission_direction
+    real :: azimuthal_angle, polar_angle
+
+    azimuthal_angle=2*3.14159265*std_uniform_distribution()
+    polar_angle=3.14159265*std_uniform_distribution()
+    random_emission_direction%x=cos(azimuthal_angle)*sin(polar_angle)
+    random_emission_direction%y=sin(azimuthal_angle)*sin(polar_angle)
+    random_emission_direction%z=cos(polar_angle)
+  end function random_emission_direction
 
 end module coordinate_type
---
+++ random.f95
@@ -0,0 +1,17 @@
+module random
+  implicit none
+contains
+  function std_uniform_distribution()
+    real :: std_uniform_distribution, rnd
+    call random_seed()
+    call random_number(rnd)
+    std_uniform_distribution=1-rnd
+  end function std_uniform_distribution
+
+  function rng(min, max)
+    real, intent(in) :: min, max
+    real :: rng
+    rng=min+std_uniform_distribution()*(max-min)
+  end function rng
+
+end module random
