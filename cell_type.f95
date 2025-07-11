0d2e32d79a5f07765be2080ce34493caffb53f07
pitkajuh <github_pj@protonmail.com>
2025-07-11 21:59:59 +0300

    ok

--- cell_type.f95
+++ cell_type.f95
@@ -35,7 +35,21 @@ contains
   function cell_test_box(this, p) result(result1)
     class(cell_box_3d), intent(inout) :: this
     type(coordinate), intent(in) :: p
-    logical :: result1
+    logical :: result1, b1, b2, b3, b4, b5, b6, result2
+    b1=.not. this%wallx_negative%surface_test(p)
+    b2=this%wallx_positive%surface_test(p)
+    b3=.not. this%wally_negative%surface_test(p)
+    b4=this%wally_positive%surface_test(p)
+    b5=.not. this%wallz_negative%surface_test(p)
+    b6=this%wallz_positive%surface_test(p)
+    result2=b1 .and. b2 .and. b3 .and. b4 .and. b4 .and. b5 .and. b6
+
+    if(result2) then
+       this%hits=this%hits+1
+       result1=.true.
+    else
+       result1=.false.
+    end if
   end function cell_test_box
 
 end module cell_type
