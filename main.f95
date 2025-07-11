8fd8b03d2bc2e5690dfdeab9b2212ade4c71713e
pitkajuh <github_pj@protonmail.com>
2025-07-11 14:55:16 +0300

    ok

--- main.f95
+++ main.f95
@@ -17,29 +17,33 @@ program main
   ! print *, endf_tape%header
   ! call endf_tape%read_tape1('cross-sections/photoat-007_N_000.endf')
 
-  ! allocate(planex :: a1)
+
+  ! call read_tape1(endf_tape, 'cross-sections/photoat-007_N_000.endf')
+  ! print *, ""
+  ! call read_tape1(endf_tape, 'cross-sections/photoat-011_Na_000.endf')
+  ! print *, ""
+  ! call read_tape1(endf_tape, 'cross-sections/photoat-022_Ti_000.endf')
+  ! print *, ""
+  ! call read_tape1(endf_tape, 'cross-sections/photoat-026_Fe_000.endf')
+  ! print *, ""
+  ! call read_tape1(endf_tape, 'cross-sections/photoat-053_I_000.endf')
+
+
+  t=std_uniform_distribution()
+  t1=std_uniform_distribution()
+  point=coordinate(t, t, t)
+  point1=coordinate(t1, t1, t1)
+  call show(point)
+  call show(point1)
+  ! print *, point-point1
+  point1=coordinate(t1, t1, t1)+100.0
+  ! print *, point+100
+  ! print *, point1
+
+  allocate(planex :: a1)
   ! call a1%create(100.0)
   ! print *, a1%value1
+  ! call a1%surface_equation(point1)
+  deallocate(a1)
 
-  call read_tape1(endf_tape, 'cross-sections/photoat-007_N_000.endf')
-  print *, ""
-  call read_tape1(endf_tape, 'cross-sections/photoat-011_Na_000.endf')
-  print *, ""
-  call read_tape1(endf_tape, 'cross-sections/photoat-022_Ti_000.endf')
-  print *, ""
-  call read_tape1(endf_tape, 'cross-sections/photoat-026_Fe_000.endf')
-  print *, ""
-  call read_tape1(endf_tape, 'cross-sections/photoat-053_I_000.endf')
-
-
-  ! t=std_uniform_distribution()
-  ! t1=std_uniform_distribution()
-  ! point=coordinate(t, t, t)
-  ! point1=coordinate(t1, t1, t1)
-  ! call show(point)
-  ! call show(point1)
-  ! print *, point-point1
-  ! point1=coordinate(t1, t1, t1)+100.0
-  ! ! print *, point+100
-  ! print *, point1
 end program main
--- surface_type.f95
+++ surface_type.f95
@@ -6,56 +6,87 @@ module surface_type
      ! For general quadratic surface S(x, y, z)=Ax^2+By^2+Cz^2+Dxy+Eyz+F zx+Gx+Hy+Iz+J
      real(kind(1.d0)) :: value1=0, A=0, B=0, C=0, D=0, E=0, F=0, G=0, H=0, I=0, J=0, L=0, M=0, K=0
    contains
-     procedure(create_interface), deferred :: create
+     ! procedure(create_interface), deferred :: create
+     procedure(create_surface_equation), deferred :: surface_equation
   end type surface
 
+  ! abstract interface
+  !    subroutine create_interface(this, v)
+  !      import surface
+  !      class(surface), intent(inout) :: this
+  !      real, intent(in) :: v
+  !    end subroutine create_interface
+
+  !    ! function create_surface_equation(this, p)
+  !    !   use coordinate_type
+  !    !   import surface
+  !    !   class(surface), intent(inout) :: this
+  !    !   type(coordinate), intent(in) :: p
+  !    ! end function create_surface_equation
+
+  ! end interface
+
   abstract interface
-     subroutine create_interface(this, v)
+     function create_surface_equation(this, p) result(result1)
+       use coordinate_type
        import surface
        class(surface), intent(inout) :: this
-       real, intent(in) :: v
-     end subroutine create_interface
+       type(coordinate), intent(in)  :: p
+       ! real :: p
+       real                          :: result1
+     end function create_surface_equation
   end interface
 
   type, extends(surface) :: planex
    contains
-     procedure, pass :: create => create_planex
+     ! procedure, pass :: create => create_planex
+     procedure, pass :: surface_equation => surface_equation_x
   end type planex
 
-  type, extends(surface) :: planey
-   contains
-     procedure, pass :: create => create_planey
-  end type planey
+  ! type, extends(surface) :: planey
+  !  contains
+  !    procedure, pass :: create => create_planey
+  ! end type planey
 
-  type, extends(surface) :: planez
-   contains
-     procedure, pass :: create => create_planez
-  end type planez
+  ! type, extends(surface) :: planez
+  !  contains
+  !    procedure, pass :: create => create_planez
+  ! end type planez
 
 contains
 
-  subroutine create_planex(this, v)
+  ! subroutine create_planex(this, v)
+  !   class(planex), intent(inout) :: this
+  !   real, intent(in) :: v
+  !   this%value1=v
+  !   this%G=1
+  !   this%J=-v
+  ! end subroutine create_planex
+
+  function surface_equation_x(this, p) result(result1)
+    ! use coordinate_type
     class(planex), intent(inout) :: this
-    real, intent(in) :: v
-    this%value1=v
-    this%G=1
-    this%J=-v
-  end subroutine create_planex
+    type(coordinate), intent(in)             :: p
+    ! real             :: p
+    real                         :: result1
+    ! result1=p%x+this%J
+  end function surface_equation_x
 
-  subroutine create_planey(this, v)
-    class(planey), intent(inout) :: this
-    real, intent(in) :: v
-    this%value1=v
-    this%H=1
-    this%J=-v
-  end subroutine create_planey
 
-  subroutine create_planez(this, v)
-    class(planez), intent(inout) :: this
-    real, intent(in) :: v
-    this%value1=v
-    this%I=1
-    this%J=-v
-  end subroutine create_planez
+  ! subroutine create_planey(this, v)
+  !   class(planey), intent(inout) :: this
+  !   real, intent(in) :: v
+  !   this%value1=v
+  !   this%H=1
+  !   this%J=-v
+  ! end subroutine create_planey
+
+  ! subroutine create_planez(this, v)
+  !   class(planez), intent(inout) :: this
+  !   real, intent(in) :: v
+  !   this%value1=v
+  !   this%I=1
+  !   this%J=-v
+  ! end subroutine create_planez
 
 end module surface_type
