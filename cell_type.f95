b3a2c2c7eff0f76feea014a956d1584de05a306e
pitkajuh <github_pj@protonmail.com>
2025-07-12 13:15:36 +0300

    ok

--- cell_type.f95
+++ cell_type.f95
@@ -9,6 +9,7 @@ module cell_type
    contains
      procedure(create_cell_test), deferred :: cell_test
      procedure(create_cell_distance), deferred :: cell_distance
+     procedure(create_initial_position), deferred :: get_initial_position
   end type cell
 
   abstract interface
@@ -27,6 +28,13 @@ module cell_type
        type(coordinate), intent(in) :: from, to
        real :: result1
      end function create_cell_distance
+
+     function create_initial_position(this) result(result1)
+       use coordinate_type
+       import cell
+       class(cell), intent(inout) :: this
+       type(coordinate) :: result1
+     end function create_initial_position
   end interface
 
   type, extends(cell) :: cell_box_3d
@@ -39,7 +47,18 @@ module cell_type
    contains
      procedure, pass :: cell_test => cell_test_box
      procedure, pass :: cell_distance => cell_distance_box
+     procedure, pass :: get_initial_position => cell_box_initial_position
   end type cell_box_3d
+
+  type, extends(cell) :: cell_cylinder_truncated_z
+     type(cylinder) :: surface_cylinder
+     type(planez) :: wallz_negative
+     type(planez) :: wallz_positive
+   contains
+     procedure, pass :: cell_test => cell_test_cylinder_z
+     procedure, pass :: cell_distance => cell_distance_cylinder_z
+     procedure, pass :: get_initial_position => cell_cylinder_z_initial_position
+  end type cell_cylinder_truncated_z
 
 contains
 
@@ -75,6 +94,7 @@ contains
     distances(5)=this%wallz_negative%surface_distance(from, to)
     distance=this%wallz_positive%surface_distance(from, to)
 
+    ! Find the distance to the surface that is the closest one, i.e. find the smallest value. The distance must be >0.
     do
        if(i==5) then
           exit
@@ -87,5 +107,71 @@ contains
     end do
     result1=distance
   end function cell_distance_box
+
+  function cell_box_initial_position(this) result(result1)
+    class(cell_box_3d), intent(inout) :: this
+    type(coordinate) :: result1
+    result1%x=rng(this%wallx_negative%value1, this%wallx_positive%value1)
+    result1%y=rng(this%wally_negative%value1, this%wally_positive%value1)
+    result1%z=rng(this%wallz_negative%value1, this%wallz_positive%value1)
+  end function cell_box_initial_position
+
+  function cell_test_cylinder_z(this, p) result(result1)
+    class(cell_cylinder_truncated_z), intent(inout) :: this
+    type(coordinate), intent(in) :: p
+    logical :: result1, b1, b2, b3, b4, b5, b6
+    ! b1=.not. this%wallx_negative%surface_test(p)
+    ! b2=this%wallx_positive%surface_test(p)
+    ! b3=.not. this%wally_negative%surface_test(p)
+    ! b4=this%wally_positive%surface_test(p)
+    ! b5=.not. this%wallz_negative%surface_test(p)
+    ! b6=this%wallz_positive%surface_test(p)
+
+    ! if(b1 .and. b2 .and. b3 .and. b4 .and. b4 .and. b5 .and. b6) then
+    !    this%hits=this%hits+1
+    !    result1=.true.
+    ! else
+    !    result1=.false.
+    ! end if
+  end function cell_test_cylinder_z
+
+  function cell_distance_cylinder_z(this, from, to) result(result1)
+    class(cell_cylinder_truncated_z), intent(inout) :: this
+    type(coordinate), intent(in) :: from, to
+    real :: result1, distance
+    real, dimension(5) :: distances
+    integer :: i=1
+    ! distances(1)=this%wallx_negative%surface_distance(from, to)
+    ! distances(2)=this%wallx_positive%surface_distance(from, to)
+    ! distances(3)=this%wally_negative%surface_distance(from, to)
+    ! distances(4)=this%wally_positive%surface_distance(from, to)
+    ! distances(5)=this%wallz_negative%surface_distance(from, to)
+    ! distance=this%wallz_positive%surface_distance(from, to)
+
+    ! ! Find the distance to the surface that is the closest one, i.e. find the smallest value. The distance must be >0.
+    ! do
+    !    if(i==5) then
+    !       exit
+    !    else if(distances(i)<distance .and. distances(i)>0) then
+    !       distance=distances(i)
+    !    else if(distance<0 .and. distances(i)>0) then
+    !       distance=distances(i)
+    !    end if
+    !    i=i+1
+    ! end do
+    ! result1=distance
+  end function cell_distance_cylinder_z
+
+  function cell_cylinder_z_initial_position(this) result(result1)
+    class(cell_cylinder_truncated_z), intent(inout) :: this
+    type(coordinate) :: result1
+    real :: radial, azimuthal_angle
+    radial=this%surface_cylinder%value1*std_uniform_distribution()**0.5
+    azimuthal_angle=2*3.14159265*std_uniform_distribution()
+
+    result1%x=radial*cos(azimuthal_angle)
+    result1%y=radial*sin(azimuthal_angle)
+    result1%z=this%wallz_negative%value1+(this%wallz_positive%value1-this%wallz_negative%value1)*std_uniform_distribution()
+  end function cell_cylinder_z_initial_position
 
 end module cell_type
--- surface_type.f95
+++ surface_type.f95
@@ -4,7 +4,7 @@ module surface_type
 
   type, abstract :: surface
      ! For general quadratic surface S(x, y, z)=Ax^2+By^2+Cz^2+Dxy+Eyz+F zx+Gx+Hy+Iz+J
-     real(kind(1.d0)) :: value1=0, A=0, B=0, C=0, D=0, E=0, F=0, G=0, H=0, I=0, J=0, L=0, M=0, K=0
+     real :: value1=0, A=0, B=0, C=0, D=0, E=0, F=0, G=0, H=0, I=0, J=0, L=0, M=0, K=0
    contains
      procedure(create_interface), deferred :: create
      procedure(create_surface_equation), deferred :: surface_equation
