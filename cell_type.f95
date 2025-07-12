c3d93b98ce8844ee9f0b50424d317f77b28345a8
pitkajuh <github_pj@protonmail.com>
2025-07-12 13:21:59 +0300

    ok

--- cell_type.f95
+++ cell_type.f95
@@ -119,20 +119,17 @@ contains
   function cell_test_cylinder_z(this, p) result(result1)
     class(cell_cylinder_truncated_z), intent(inout) :: this
     type(coordinate), intent(in) :: p
-    logical :: result1, b1, b2, b3, b4, b5, b6
-    ! b1=.not. this%wallx_negative%surface_test(p)
-    ! b2=this%wallx_positive%surface_test(p)
-    ! b3=.not. this%wally_negative%surface_test(p)
-    ! b4=this%wally_positive%surface_test(p)
-    ! b5=.not. this%wallz_negative%surface_test(p)
-    ! b6=this%wallz_positive%surface_test(p)
+    logical :: result1, b1, b2, b3
+    b1=.not. this%surface_cylinder%surface_test(p)
+    b2=this%wallz_positive%surface_test(p)
+    b3=.not. this%wallz_negative%surface_test(p)
 
-    ! if(b1 .and. b2 .and. b3 .and. b4 .and. b4 .and. b5 .and. b6) then
-    !    this%hits=this%hits+1
-    !    result1=.true.
-    ! else
-    !    result1=.false.
-    ! end if
+    if(b1 .and. b2 .and. b3) then
+       this%hits=this%hits+1
+       result1=.true.
+    else
+       result1=.false.
+    end if
   end function cell_test_cylinder_z
 
   function cell_distance_cylinder_z(this, from, to) result(result1)
