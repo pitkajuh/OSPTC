5b1d8a4ae55933e5828a431cb69e8847aafa7574
pitkajuh <github_pj@protonmail.com>
2025-07-13 16:41:19 +0300

    ok

--- tape_type.f95
+++ tape_type.f95
@@ -25,36 +25,28 @@ contains
 
   subroutine read_begin1(this, z, ios)
     type(tape) :: this
-    integer :: z
-    integer :: ios
+    integer :: z, ios, i
     character(75) :: line
-    integer :: n
 
     real(kind(1.d0)) :: ZA, AWR
     integer :: L1, L2, N1, N2, MAT, MF, MT, MT1, MF1
 
-    n=1
-
     read(z, *, iostat=ios) line
 
-    do
+    do i=1, 4
        read(z, '(2E11.0,4I11,I4,I2,I3,I5)', iostat=ios) ZA, AWR, L1, L2, N1, N2, MAT, MF, MT
-       this%header(1, n)=ZA
-       this%header(2, n)=AWR
-       this%header(3, n)=L1
-       this%header(4, n)=L2
-       this%header(5, n)=N1
-       this%header(6, n)=N2
-       if(n==4) exit
-       n=n+1
+       this%header(1, i)=ZA
+       this%header(2, i)=AWR
+       this%header(3, i)=L1
+       this%header(4, i)=L2
+       this%header(5, i)=N1
+       this%header(6, i)=N2
     end do
 
     do
        read(z, '(A65,I1)', iostat=ios) line, N1
        if(N1==3)  exit
     end do
-
-    n=0
 
     do
        read(z, '(I36,I10,I10,I10)', iostat=ios) N1, MAT, MF, MT
