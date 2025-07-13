295df4e4b81d0632df9569c6d3627a1557dbed1c
pitkajuh <github_pj@protonmail.com>
2025-07-13 13:03:10 +0300

    ok

--- section_type.f95
+++ section_type.f95
@@ -5,6 +5,7 @@ module section_type
      real, dimension(6, 3) :: header
    contains
      procedure, public :: read_section_header
+     procedure, public :: read_section
   end type section
 
 
@@ -44,14 +45,14 @@ contains
     n=1
   end subroutine read_section_header
 
-  !   subroutine read_section(this, z, ios, MF,  MT)
-  !     type(section) :: this
-  !     real :: v1, v2, v3, v4, v5, v6
-  !     integer :: L1, L2, N1, N2, MAT, MF, MT, z, ios
+  subroutine read_section(this, z, ios, MF,  MT)
+    class(section), intent(inout) :: this
+    real :: v1, v2, v3, v4, v5, v6
+    integer :: L1, L2, N1, N2, MAT, MF, MT, z, ios
 
-  !     do
-  !        read(z, '(6E11.0,I4,I2,I3)', iostat=ios) v1, v2, v3, v4, v5, v6, MAT, MF, MT
-  !        if(MT==0) exit
-  !     end do
-  !   end subroutine read_section
+    do
+       read(z, '(6E11.0,I4,I2,I3)', iostat=ios) v1, v2, v3, v4, v5, v6, MAT, MF, MT
+       if(MT==0) exit
+    end do
+  end subroutine read_section
 end module section_type
