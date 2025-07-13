bf5312792e1e236937ac1a801c9de3eb1e52cc22
pitkajuh <github_pj@protonmail.com>
2025-07-13 22:01:39 +0300

    ok

--- section_type.f95
+++ section_type.f95
@@ -108,25 +108,25 @@ contains
   subroutine read_section(this, z, ios, MF,  MT, n, records)
     class(section), intent(inout) :: this
     real, allocatable :: records(:, :)
-    real :: v1, v2, v3, v4, v5, v6
+    real :: e1, v2, e2, v4, e3, v6
     integer :: L1, L2, N1, N2, MAT, MF, MT, z, ios, n, i
     i=1
 
     do
-       read(z, '(6E11.0,I4,I2,I3)', iostat=ios) v1, v2, v3, v4, v5, v6, MAT, MF, MT
+       read(z, '(6E11.0,I4,I2,I3)', iostat=ios) e1, v2, e2, v4, e3, v6, MAT, MF, MT
        if(MT==0) exit
-       print *, v1, v2, v3, v4, v5, v6
-       ! this%records(i, 1)=v1
+       print *, e1, v2, e2, v4, e3, v6
+       ! this%records(i, 1)=e1
        ! this%records(i, 2)=v2
-       ! this%records(i+1, 1)=v3
+       ! this%records(i+1, 1)=e2
        ! this%records(i+1, 2)=v4
-       ! this%records(i+2, 1)=v5
+       ! this%records(i+2, 1)=e3
        ! this%records(i+2, 2)=v6
-       records(i, 1)=v1
+       records(i, 1)=e1
        records(i, 2)=v2
-       records(i+1, 1)=v3
+       records(i+1, 1)=e2
        records(i+1, 2)=v4
-       records(i+2, 1)=v5
+       records(i+2, 1)=e3
        records(i+2, 2)=v6
        i=i+1
     end do
