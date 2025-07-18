a0071ee28b658229b44aafc6a53ecfbe9a070162
pitkajuh <github_pj@protonmail.com>
2025-07-18 18:23:21 +0300

    ok

--- photon_angular_distribution.f95
+++ photon_angular_distribution.f95
@@ -95,7 +95,7 @@ contains
     real(kind(1.d0)), intent(in), allocatable :: real_factor(:, :)
     real(kind(1.d0)), intent(in), allocatable :: imaginary_factor(:, :)
     integer, intent(in) :: n1, n2, n3, n4
-    real(kind(1.d0)) :: Fprime, Fprimeprime, energy, deltamu, a1, e, mu, x1, deltae, hc, elim, f, deltax
+    real(kind(1.d0)) :: Fprime, Fprimeprime, energy, deltamu, a1, e, mu, x1, deltae, hc, elim, f, deltax, x2
     integer :: i, j
     hc=4.135667696e-15_8*299792458.0_8
     energy=10.0_8
@@ -103,27 +103,25 @@ contains
     ! F=linear_interpolation(coherent_factor, x(energy, mu), n2)
     ! Fprime=linear_interpolation(real_factor, energy, n3)
     ! Fprimeprime=linear_interpolation(imaginary_factor, energy, n4)
-    deltae=100!coherent_factor(1, n2)/n2
+    deltae=100_8!coherent_factor(1, n2)/n2
     e=deltae
-    mu=0.0
-    deltamu=-1/n2
+    mu=0.0_8
+    deltamu=-1.0_8/n2
     elim=2.00E6_8
-    x1=0.0
+    x1=0.0_8
     deltax=x(deltae, 1.0_8/n2)
     print *, elim/deltae
     do i=1, n2
 
        do
           if(e==elim) exit
-          ! x1=(e/hc)*((1-mu)/2)**0.5
-          f=F1(x(e, mu), coherent_factor, n2)
-          ! a1=0.5*deltax*(coherent_factor(2, i)+coherent_factor(2, i-1))
-          a1=0.5*deltax*(F1(x(e, mu), coherent_factor, n2)+F1(x(e-deltae, mu-deltamu), coherent_factor, n2))
-          ! print *, i, j, x1, e
+          x2=x(e, mu)
+          a1=0.5*deltax*(F1(x2, coherent_factor, n2)+F1(x2-deltax, coherent_factor, n2))
+          ! print *, e, a1
           e=e+deltae
        end do
        exit
-       ! e=deltae
+       e=deltae
        mu=mu+deltamu
     end do
     ! ! deltax=coherent_factor(2, n2)
