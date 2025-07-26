6dc3da372d41bdba1f6168bd95b67865f666fd00
pitkajuh <github_pj@protonmail.com>
2025-07-26 13:24:47 +0300

    coherent scattering

--- physics_routine.f95
+++ physics_routine.f95
@@ -11,7 +11,7 @@ contains
     real(kind(1.d0)), intent(in), allocatable :: A(:, :)
     real(kind(1.d0)), intent(in) :: energy
     integer, intent(in) :: n, n2, n3
-    real(kind(1.d0)) :: hc, ymax, deltay, a1, x2, Amax, Ainverse, Avalue, rand, energymin, ymin
+    real(kind(1.d0)) :: hc, ymax, deltay, a1, x2, Amax, Ainverse, Avalue, rand, energymin, ymin, sum1, mu
     integer :: i
     hc=4.135667696e-15_8*299792458.0_8
     ymax=(energy/hc)**2
@@ -19,19 +19,53 @@ contains
     ymin=(energymin/hc)**2
     deltay=(ymax-ymin)/n3
     rand=std_uniform_distribution()
+    mu=0.0_8
+    ! Amax=0.0_8
+
+    ! Amax=Amax+0.5_8*deltay*(F1((n*deltay)**0.5_8, coherent_factor, n2)+&
+    !      F1(A(1, 2)**0.5_8, coherent_factor, n2))
+
+    ! do i=3, n
+    !    Amax=Amax+deltay*F1(A(1, i)**0.5_8, coherent_factor, n2)
+    !    ! write(new1, *) A(1, i), ";", A(2, i)
+    !    ! if(i<5) print *, A(1, i), A(2, i)
+    !    ! print *, i,n,A(1, i), A(2, i)
+    ! end do
 
     Amax=linear_interpolation(A, ymax, n)
-    Avalue=rand*Amax
+    ! Avalue=Amax
 
-    do i=1, n3
-       if(A(2, i)<=Avalue .and. Avalue<A(2, i+1)) exit
+    do
+       Avalue=rand*Amax
+
+       do i=1, n3
+          if(A(2, i)<=Avalue .and. Avalue<A(2, i+1)) exit
+       end do
+
+       Ainverse=A(1, i)+(Avalue-A(2, i))*((A(1, i+1)-A(1, i))/(A(2, i+1)-A(2, i)))
+       mu=1-2*Ainverse/ymax
+
+       if(rand<(1+mu)/2) exit
+       rand=std_uniform_distribution()
     end do
+
+    ! Avalue=rand*Amax
+
+    ! do i=1, n3
+    !    if(A(2, i)<=Avalue .and. Avalue<A(2, i+1)) exit
+    ! end do
+
+    ! Ainverse=A(1, i)+(Avalue-A(2, i))*((A(1, i+1)-A(1, i))/(A(2, i+1)-A(2, i)))
 
     print *, energy, i-1, A(1, i-1), A(2, i-1), Avalue
     print *, energy, i, A(1, i), A(2, i), Avalue
-    print *, "result", ((Avalue-A(2, i-1))/(A(2, i)-A(2, i-1)))*(A(1, i)-A(1, i-1))+A(1, i-1), Avalue
+    ! print *, "result", ((Avalue-A(2, i-1))/(A(2, i)-A(2, i-1)))*(A(1, i)-A(1, i-1))+A(1, i-1), Avalue, "inverse", Ainverse, "angle", mu
+    print *, "angle", mu
 
     print *, " "
+
+    ! rand=std_uniform_distribution()
+    ! if(rand>(1+(1-2*Ainverse/ymax))) print *, "not accepted"
   end function coherent_scattering_reaction
 
   subroutine sum1(limits, records, energy, n, total, i)
