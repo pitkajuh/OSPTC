d2d75df8b64bc1e2b1d9e6123c7da781e1029920
pitkajuh <github_pj@protonmail.com>
2026-04-14 13:24:47 +0300

    binary search

--- interpolate.f95
+++ interpolate.f95
@@ -1,50 +1,13 @@
 module interpolate
+  use search
   implicit none
 contains
-
   function linear_interpolation(array, x, nx) result(r)
-    integer :: nx, i, L1, R1
+    integer :: nx, i
     real(kind(1.d0)), intent(in) :: x
-    real(kind(1.d0)) :: r, A1
+    real(kind(1.d0)) :: r
     real(kind(1.d0)), intent(in), dimension(:, :) :: array
-
-    r=0.0_8
-    L1=0
-    R1=nx-1
-    ! print *, "AOE"
-    do
-       if(L1>=R1) then
-          exit
-       end if
-
-       A1=(R1-L1)/2
-       i=L1+floor(A1)
-
-       if(array(1, i)<x) then
-          L1=i+1
-       else if(array(1, i)>x) then
-          R1=i-1
-       else
-          exit
-       end if
-    end do
-
-
-    ! do i=1, nx
-    !    ! print *, i
-    !    if(array(1, i)>=x) exit
-    ! end do
-
-
-    ! if(i>=nx) then
-    !    i=i-1
-    !    r=array(2, i-1)+(array(2, i)-array(2, i-1))*((x-array(1, i-1))/(array(1, i)-array(1, i-1)))
-    ! else
-
-
-       r=array(2, i-1)+(x-array(1, i-1))*(array(2, i)-array(2, i-1))/(array(1, i)-array(1, i-1))
-
-
-    ! endif
+    i=binary_search(array, x, nx)
+    r=array(2, i-1)+(x-array(1, i-1))*(array(2, i)-array(2, i-1))/(array(1, i)-array(1, i-1))
   end function linear_interpolation
 end module interpolate
---
+++ search.f95
@@ -0,0 +1,32 @@
+module search
+  implicit none
+contains
+  function binary_search(array, x, nx) result(i)
+    integer :: nx, i, left, right
+    real(kind(1.d0)), intent(in) :: x
+    real(kind(1.d0)) :: r, A1
+    real(kind(1.d0)), intent(in), dimension(:, :) :: array
+
+    r=0.0_8
+    left=0
+    right=nx-1
+
+    do
+       if(left>=right) then
+          exit
+       end if
+
+       A1=(right-left)/2
+       i=left+floor(A1)
+
+       if(array(1, i)<x) then
+          left=i+1
+       else if(array(1, i)>x) then
+          right=i-1
+       else
+          exit
+       end if
+    end do
+
+  end function binary_search
+end module search
