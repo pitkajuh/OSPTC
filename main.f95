12c54b66e0b8f1971ed15298f1d4baa7d758e971
pitkajuh <github_pj@protonmail.com>
2026-06-29 16:45:01 +0300

    ok

--- main.f95
+++ main.f95
@@ -9,45 +9,61 @@ program main
   use photon_type
   implicit none
 
-  integer :: i, second
+  integer :: i, j, second, k
   class(cell), allocatable :: source_cell, cell2
   class(material), allocatable :: steel1
   class(material), allocatable :: nitrogen1
   type(coordinate) :: new_location, centered_at
   type(photon) :: ph
   class(radionuclide), allocatable :: co_60_source
-
+  logical :: cell_hit
   class(cells), allocatable :: cell_all(:)
+  cell_hit=.false.
+  k=1
+  allocate(steel :: steel1)
+  call steel1%create()
+  allocate(nitrogen :: nitrogen1)
+  call nitrogen1%create()
+
   allocate(cell_all(2))
 
   allocate(cell_cylinder_truncated_z::cell_all(1)%cell_array)
   select type(cell_array => cell_all(1)%cell_array)
   class is (cell_cylinder_truncated_z)
-     ! cell_array%cell_material=steel1
+     cell_array%name="source"
+     cell_array%cell_material=steel1
      call cell_array%create(0.5_8, -1.0_8, 1.0_8, 0.0_8, 0.0_8, 0.0_8)
   end select
 
   allocate(cell_cylinder_truncated_z::cell_all(2)%cell_array)
   select type(cell_array => cell_all(2)%cell_array)
   class is (cell_cylinder_truncated_z)
-     ! cell_array%cell_material=nitrogen1
+     cell_array%name="outside"
+     cell_array%cell_material=nitrogen1
      call cell_array%create(20.0_8, -20.0_8, 20.0_8, 0.0_8, 0.0_8, 0.0_8)
   end select
 
-  print *, size(cell_all)
+  ! print *, size(cell_all)
 
-  do i=1, size(cell_all)
-     select type(cell_array => cell_all(i)%cell_array)
-        ! print *,cell_array
-        ! cell_all%cell_array(i)!%surface_cylinder%v
-     end select
-  end do
+  ! new_location=coordinate(0.1_8, 0.1_8, 0.1_8)
+  ! print *, source_cell%cell_test(new_location)
 
 
+  ! do i=1, size(cell_all)
+  !    ! select type(cell_array => cell_all(i)%cell_array)
+
+  !    !    ! print *,cell_array
+  !    !    ! cell_all%cell_array(i)!%surface_cylinder%v
+  !    ! end select
+  !    print *, cell_all(i)%cell_array%name
+  !    print *, cell_all(i)%cell_array%cell_test(new_location)
+  ! end do
 
 
-  ! allocate(co_60 :: co_60_source)
-  ! co_60_source%activity=1E3
+
+
+  allocate(co_60 :: co_60_source)
+  co_60_source%activity=1E3
   ! allocate(steel :: steel1)
   ! call steel1%create()
   ! allocate(cell_cylinder_truncated_z :: source_cell)
@@ -92,26 +108,34 @@ program main
   ! ! allocate(cell_cylinder_truncated_z :: cells(1))
   ! ! allocate(cell_cylinder_truncated_z :: cells(2))
 
-  ! ! do second=1, 1000
-  ! !    do i=1, co_60_source%activity
+  do second=1, 1000
+     print *, second, "s"
 
-  !       ph=co_60_source%pdf()
-  !       ph%origin=source_cell%random_initial_position()
+     do i=1, co_60_source%activity
+        ph=co_60_source%pdf()
+        ph%origin=cell_all(1)%cell_array%random_initial_position()
 
-  !       new_location=calculate_mfp(ph, source_cell%cell_material%get_mu_value(ph%energy))
-  !       ph%origin=new_location
-  !       call show(new_location)
-  !       print *, source_cell%cell_test(new_location)
-  !       print *, cell2%cell_test(new_location)
-  !       ! print *, steel1%get_mu_value(ph%energy), ph%energy/1E6_8
-  !       ! call show(new_location)
-  !       !  E=1E1_8
-  !       call reaction_function(steel1%endf, ph)
-  !       ! print *, second, i, E
-  ! !    end do
-  ! ! end do
+        do j=1, size(cell_all)
+           new_location=calculate_mfp(ph, cell_all(j)%cell_array%cell_material%get_mu_value(ph%energy))
+           ph%origin=new_location
+           cell_hit=cell_all(j)%cell_array%cell_test(new_location)
 
-  ! deallocate(co_60_source)
+           if(cell_hit .eqv. .true.) then
+              k=j
+              exit
+           end if
+        end do
+
+        ! Photon did not hit any cell. It left the geometry.
+        if(cell_hit .eqv. .false.) then
+           continue
+        end if
+
+        call reaction_function(cell_all(k)%cell_array%cell_material%endf, ph)
+     end do
+  end do
+
+  deallocate(co_60_source)
   ! ! call clear_material(steel1)
   ! deallocate(steel1)
   ! deallocate(nitrogen1)
@@ -124,4 +148,5 @@ program main
   ! deallocate(source_cell)
   ! deallocate(cell2)
   ! ! call clear_seed()
+  deallocate(cell_all)
 end program main
